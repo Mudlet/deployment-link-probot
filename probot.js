893da58aec9c3ab42a98310db81521e683f4a383
Michael Edwards <mledwards@carilionclinic.org>
2025-07-22 12:12:27 -0400

    make APP_ID a number

--- probot.js
+++ probot.js
@@ -5,7 +5,7 @@ console.log("APP_ID:", process.env.APP_ID);
 module.exports = new Probot({
   overrides: {
     // injected by vercel env vars
-    appId: process.env.APP_ID,
+    appId: Number(process.env.APP_ID),
     privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
     secret: process.env.WEBHOOK_SECRET
   },
