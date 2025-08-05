c164905c2d709fb71f0dabc48700332b834af3f9
Michael Edwards <mledwards@carilionclinic.org>
2025-08-05 07:46:45 -0400

    removing await because of an error

--- api/github/webhooks.js
+++ api/github/webhooks.js
@@ -1,14 +1,13 @@
-const { createNodeMiddleware, Probot } = require('probot');
-const { probotApp } = require('../../index');
+const { createNodeMiddleware, Probot } = require('probot')
+const { probotApp } = require('../../index')
 
 const probot = new Probot({
   appId: Number(process.env.APP_ID ?? 0),
   privateKey: (process.env.PRIVATE_KEY ?? '').replace(/\\n/g, '\n'),
-  secret: process.env.WEBHOOK_SECRET ?? '',
-});
+  secret: process.env.WEBHOOK_SECRET ?? ''
+})
 
-// ✅ Use `await` to export the middleware directly
-module.exports = await createNodeMiddleware(probotApp, {
+module.exports = createNodeMiddleware(probotApp, {
   probot,
-  webhooksPath: '/api/github/webhooks',
-});
+  webhooksPath: '/api/github/webhooks'
+})
