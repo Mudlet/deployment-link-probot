12a0bf2bf22dddbf4880489f4d70e3037ae94b33
keneanung <keneanung@googlemail.com>
2025-10-28 10:21:19 +0000

    await the creation of the node middleware

--- api/github/webhooks.js
+++ api/github/webhooks.js
@@ -3,4 +3,4 @@ import { createNodeMiddleware, createProbot } from "probot";
 import {appFunction} from "../../index.js";
 const probot = createProbot();
 
-export default createNodeMiddleware(appFunction, { probot, webhooksPath: '/api/github/webhooks' });
+export default await createNodeMiddleware(appFunction, { probot, webhooksPath: '/api/github/webhooks' });
